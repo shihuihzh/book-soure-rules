131bed1975b3e3a8f53f913a10356403c3aee206
Howe Huang <shihuihzh@gmail.com>
2023-11-17 13:19:42 +0800

    mostly support for dom

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import fs from 'fs/promises'
 import biquSource from './biqu'
 import { BookSource, SearchResult } from './types'
 import { JSDOM } from 'jsdom'
-import { analyzeDomStep } from './utils'
+import { analyzeDomStep, makeIndexesFromRange, makeIndexesNonNegative } from './utils'
 
 function debug(log: string) {
   console.log(log)
@@ -11,27 +12,20 @@ class Source {
   constructor(public bookSource: BookSource) {}
 
   extractDataByDomRule(html: string, rule?: string): Array<string | string[]> {
-    debug(`rule: ${rule}`)
     if (!rule || !html) {
       return []
     }
 
     const dom = new JSDOM(html)
     const doc = dom.window.document
-    const queryBySelector = (targetElements: Node | Array<Node>, selector: string, reverse: boolean) => {
-      const es =
-        targetElements instanceof Array
-          ? targetElements.flatMap((e) => Array.from((e as Element).querySelectorAll(selector)))
-          : Array.from((targetElements as Element).querySelectorAll(selector))
+    const queryBySelector = (targetElements: Array<Node>, selector: string, reverse: boolean) => {
+      const es = targetElements.flatMap((e) => Array.from((e as Element).querySelectorAll(selector)))
 
       return reverse ? es.reverse() : es
     }
 
-    const allChildren = (targetElements: Node | Array<Node>, reverse: boolean) => {
-      const es =
-        targetElements instanceof Array
-          ? (targetElements.flatMap((e) => Array.from(e.childNodes)) as Element[])
-          : (Array.from(targetElements.childNodes) as Element[])
+    const allChildren = (targetElements: Array<Node>, reverse: boolean) => {
+      const es = targetElements.flatMap((e) => Array.from(e.childNodes)) as Element[]
 
       return reverse ? es.reverse() : es
     }
@@ -39,92 +33,78 @@ class Source {
     let result: Array<string | string[]> = []
 
     const steps = rule.split('@')
-    debug(`rule steps: ${steps}`)
-    let targetElements: Node | Array<Node> | Array<Node[]> = doc
-    let emptyTextNode = doc.createTextNode('')
+    const emptyTextNode = doc.createTextNode('')
+    debug(`all rule steps: ${steps}`)
+    let targetElements: Array<Node> | Array<Node[]> = [doc]
 
     let lastReplaceRegex, lastReplaceTargetStr: string
     for (let i = 0; i < steps.length; i++) {
-      const { type, name, includeIndex, excludeIndex, reverse, replaceRegex, replaceTargetStr } = analyzeDomStep(steps[i])
+      const step = steps[i]
+      const { type, name, includeIndex, excludeIndex, reverse, replaceRegex, replaceTargetStr, isRange, rangeStart, rangeEnd, rangeStep } =
+        analyzeDomStep(step)
       lastReplaceRegex = replaceRegex
       lastReplaceTargetStr = replaceTargetStr
 
-      debug(`run rule: type:${type}, selector: ${name}, reveres: ${reverse} includeIndex: [${includeIndex}] excludeIndex: [${excludeIndex}]`)
+      debug(
+        `run rule step:{${step}} type:${type}, selector: ${name}, reveres: ${reverse} includeIndex: [${includeIndex}] excludeIndex: [${excludeIndex}]`
+      )
 
       switch (type.toLowerCase()) {
         case 'class':
-          targetElements = queryBySelector(targetElements as Node | Array<Node>, `.${name}`, reverse)
+          targetElements = queryBySelector(targetElements as Array<Node>, `.${name}`, reverse)
           break
         case 'id':
-          targetElements = queryBySelector(targetElements as Node | Array<Node>, `#${name}`, reverse)
+          targetElements = queryBySelector(targetElements as Array<Node>, `#${name}`, reverse)
           break
         case 'tag':
-          targetElements = queryBySelector(targetElements as Node | Array<Node>, name, reverse)
+          targetElements = queryBySelector(targetElements as Array<Node>, name, reverse)
           break
         case 'children':
-          targetElements = allChildren(targetElements as Node | Array<Node>, reverse)
+          targetElements = allChildren(targetElements as Array<Node>, reverse)
           break
 
         // below are result case
-        case 'text':
-          // result =
-          //   targetElements instanceof Array
-          //     ? targetElements.map((e) => e.textContent || '')
-          //     : [(targetElements as unknown as HTMLElement).textContent || '']
-          break
-
         case 'textnodes':
-          targetElements =
-            targetElements instanceof Array
-              ? targetElements.map((e) => Array.from((e as HTMLElement).childNodes).filter((c) => c.nodeType === 3))
-              : [Array.from((targetElements as unknown as HTMLElement).childNodes).filter((c) => c.nodeType === 3)]
+          targetElements = (targetElements as Node[]).map((e) => Array.from((e as Element).childNodes).filter((c) => c.nodeType === 3))
           break
 
         case 'owntext':
-          targetElements =
-            targetElements instanceof Array
-              ? targetElements.flatMap((e) => Array.from((e as HTMLElement).childNodes).filter((c) => c.nodeType === 3))
-              : Array.from((targetElements as unknown as HTMLElement).childNodes).filter((c) => c.nodeType === 3)
+          targetElements = (targetElements as Node[]).flatMap((e) => Array.from((e as HTMLElement).childNodes).filter((c) => c.nodeType === 3))
 
         case 'href':
-          targetElements =
-            targetElements instanceof Array
-              ? (targetElements as Node[]).map((e) => (e as Element).attributes.getNamedItem('href') || emptyTextNode)
-              : [(targetElements as unknown as Element).attributes.getNamedItem('href') || emptyTextNode]
-          break
-
         case 'src':
-          targetElements =
-            targetElements instanceof Array
-              ? (targetElements as Node[]).map((e) => (e as Element).attributes.getNamedItem('src') || emptyTextNode)
-              : [(targetElements as unknown as Element).attributes.getNamedItem('src') || emptyTextNode]
+          targetElements = (targetElements as Node[]).map((e) => (e as Element).attributes.getNamedItem(type) || emptyTextNode)
           break
         case 'html':
-          targetElements =
-            targetElements instanceof Array
-              ? (targetElements as Node[]).map((e) => doc.createTextNode((e as Element).innerHTML || ''))
-              : [doc.createTextNode((targetElements as Element).innerHTML || '')]
+          targetElements = (targetElements as Node[]).map((e) => doc.createTextNode((e as Element).innerHTML || ''))
           break
         case 'all':
           // what's all means.
           break
       }
 
-      // if (targetElements.length === 0) {
-      //   // not found, break the loop
-      //   break
-      // }
+      if (targetElements.length === 0) {
+        // not found, break the loop
+        break
+      }
 
       // include or exclude elements
-      if (includeIndex.length > 0 || excludeIndex.length > 0) {
-        if (targetElements instanceof Array) {
+      if (includeIndex.length > 0 || excludeIndex.length > 0 || isRange) {
+        const size = targetElements.length
+
+        if (isRange) {
+          let rangeIncludeIndex = makeIndexesFromRange(size, rangeStart, rangeEnd, rangeStep)
+          targetElements = rangeIncludeIndex.map((i) => (targetElements as Node[])[i])
+        } else {
+          const nonNegativeIncludeIndex = makeIndexesNonNegative(size, includeIndex)
+          const nonNegativeExcludeIndex = makeIndexesNonNegative(size, excludeIndex)
           targetElements = (targetElements as Node[]).filter((_, i) => {
-            if (excludeIndex.includes(i)) {
-              return false
+            if (nonNegativeIncludeIndex.length > 0) {
+              return nonNegativeIncludeIndex.includes(i)
             }
 
-            if (includeIndex.includes(i)) {
-              return true
+            if (nonNegativeExcludeIndex.includes(i)) {
+              return false
             }
 
             return true
@@ -133,10 +113,8 @@ class Source {
       }
     }
 
-    result =
-      targetElements instanceof Array
-        ? targetElements.map((e) => (e instanceof Array ? e.map((ee) => ee.textContent || '') : e.textContent || ''))
-        : [(targetElements as Element).textContent || '']
+    // all get `text`
+    result = targetElements.map((e) => (e instanceof Array ? e.map((ee) => ee.textContent || '') : e.textContent || ''))
 
     // handle content
     if (lastReplaceRegex) {
@@ -150,6 +128,11 @@ class Source {
       })
     }
     return result
+  }
+
+  async test(html: string, rule: string) {
+    const text = this.extractDataByDomRule(html, rule)
+    console.log(text)
   }
 
   async search(key: string): Promise<Partial<SearchResult>[]> {
@@ -167,7 +150,7 @@ class Source {
     for (let i = 0; i < size; i++) {
       const result: Partial<SearchResult> = {}
       for (let j = 0; j < keys.length; j++) {
-        result[keys[j]] = rawResults[keys[j]][i] as string
+        result[keys[j]] = (rawResults[keys[j]]?.[i] as string) || ''
       }
 
       searchResults.push(result)
@@ -192,13 +175,17 @@ async function request(url: string, options?: { method?: 'string'; headers?: Rec
   return resp.text()
 }
 
-function main() {
+async function main() {
   const source = new Source(biquSource)
-  source.search('凡人修仙传').then(e => console.log(JSON.stringify(e)))
+  // source.search('凡人修仙传').then(e => console.log(JSON.stringify(e)))
   //
   // 'class.novelslist2@tag.li!0'.split('@').forEach((e) => {
   //   console.log(analyzeDomStep(e))
   // })
+  //
+  // source.test(await fs.readFile('./test/search.html', 'utf-8'), 'class.s2@tag.a[10:0:-2]@text')
+  //
+  // console.log(analyzeDomStep('[1:100]'))
 }
 
 main()
--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,7 @@ export const analyzeDomStep = (step: string) => {
   let includeIndex: number[] = []
   let rangeStart: number = 0
   let rangeEnd: number = 0
+  let rangeStep: number = 1
   let isExclude = false
   let isRange = false
   let hasReplace = false
@@ -30,17 +31,25 @@ export const analyzeDomStep = (step: string) => {
       case '.':
       case '!':
       case '#':
+      case '[':
+      case ']':
         if (c === '!') {
           isExclude = true
-        }
-
-        if (c === '#') {
+          if (candiate.startsWith('[')) {
+            candiate += c // eating indexes, continue
+            break
+          }
+        } else if (c === '#') {
           hasReplace = true
           endLoop = true
         }
 
         tokens.push(candiate)
         candiate = ''
+
+        if (c === '[') {
+          candiate += c
+        }
         break
       default:
         candiate += c
@@ -52,12 +61,42 @@ export const analyzeDomStep = (step: string) => {
     tokens.push(candiate)
   }
 
-  const [type, name, posistion] = tokens
+  let [type, name, posistion] = tokens
+  console.log(tokens)
+  
+  if (!type && !posistion) { // index mode
+    type = 'children'
+    posistion = name
+    name = ''
+  }
+
   if (posistion) {
-    if (isExclude) {
-      excludeIndex.push(parseInt(posistion))
+    if (posistion.startsWith('[')) {
+      const posistionArray = posistion.substring(isExclude ? 2 : 1)
+      if (posistionArray.includes(':')) {
+        // range
+        isRange = true
+        const [start, end, step] = posistionArray.split(':')
+        rangeStart = parseInt(start || '0')
+        rangeEnd = parseInt(end || '-1')
+        rangeStep = parseInt(step || '1')
+      } else {
+        // indexes
+        const indexes = posistionArray.split(',')
+        indexes.forEach((index) => {
+          if (isExclude) {
+            excludeIndex.push(parseInt(index))
+          } else {
+            includeIndex.push(parseInt(index))
+          }
+        })
+      }
     } else {
-      includeIndex.push(parseInt(posistion))
+      if (isExclude) {
+        excludeIndex.push(parseInt(posistion))
+      } else {
+        includeIndex.push(parseInt(posistion))
+      }
     }
   }
   if (hasReplace) {
@@ -73,9 +112,37 @@ export const analyzeDomStep = (step: string) => {
     isRange,
     rangeStart,
     rangeEnd,
+    rangeStep,
     includeIndex,
     excludeIndex,
     replaceRegex,
     replaceTargetStr,
   }
+}
+
+export function makeIndexesNonNegative(size: number, indexArray: number[]) {
+  // size 10
+  // -1 => 9
+  // -10 => 0
+  // -11 => -1 ? let it => 0
+  // >= 10 let it to 9
+  return indexArray.map((i) => (i < 0 ? (size + i > 0 ? size + i : 0) : i >= size ? size - 1 : i))
+}
+
+export function makeIndexesFromRange(size: number, rangeStart: number, rangeEnd: number, rangeStep: number) {
+  const indexes = []
+  let [pRangeStart, pRangeEnd] = makeIndexesNonNegative(size, [rangeStart, rangeEnd])
+  const step = Math.abs(rangeStep)
+
+  if (pRangeStart > pRangeEnd) {
+    for (let i = pRangeStart; i >= pRangeEnd; i -= step) {
+      indexes.push(i)
+    }
+  } else {
+    for (let i = pRangeStart; i <= pRangeEnd; i += step) {
+      indexes.push(i)
+    }
+  }
+
+  return indexes
 }
